f4624af87a772605dd5893736debcfd0f8a0d323
Edward Alexandroff <alexandroffe@gmail.com>
2023-12-14 16:37:04 +0000

    Test for Network's distant_neighbours method

--- test_network.py
+++ test_network.py
@@ -1,9 +1,8 @@
 import pytest
 from network import Network
-#from ..londontube.network import Network 
+#from ..londontube.network import Network #import Network according to the directory structure given in the assignment pdf
 
 def test_network_n_nodes():
-    
     #test matrix taken from eqn 1 in the assignment pdf
     test_adjacency_matrix = [[0, 1, 0, 3],
                              [1, 0, 2, 1],
@@ -45,5 +44,24 @@ def test_network_add():
     subnetwork_1 = Network(subnetwork_adjacency_matrix_1)
     subnetwork_2 = Network(subnetwork_adjacency_matrix_2)
     assert (subnetwork_1 + subnetwork_2).adjacency_matrix == supernetwork_adjacency_matrix
-    
+
+
+@pytest.mark.parametrize("n, v, neighbors", [
+    (1, 0, [1,3]), #testing nearest neighbors
+    (2, 1, [0, 2, 3]), #testing second-nearest neighbors
+    (3, 1, [0, 2, 3]), #testing third-nearest neighbors
+])
+def test_distant_neighbours(n, v, neighbors):
+    #test matrix taken from eqn 1 in the assignment pdf
+    test_adjacency_matrix = [[0, 1, 0, 3],
+                             [1, 0, 2, 1],
+                             [0, 2, 0, 0],
+                             [3, 1, 0, 0 ]]
+    test_network = Network(test_adjacency_matrix)
+
+    nearest_neighbours = set(test_network.distant_neighbours(n, v))
+    expected_nearest_neighbors = set(neighbors)
+
+    assert nearest_neighbours == expected_nearest_neighbors
+
 # negative tests ensuring that the validity of inputs/raised errors work as expected:
