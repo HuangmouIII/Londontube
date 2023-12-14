87dab246c4500f76c38ef11b528fb37de05844b1
Edward Alexandroff <alexandroffe@gmail.com>
2023-12-14 13:21:27 +0000

    Added test for Network's adjacency_matrix method

--- test_network.py
+++ test_network.py
@@ -15,6 +15,16 @@ def test_network_n_nodes():
     n_nodes = test_network.n_nodes
     assert expected_n_nodes == n_nodes
 
+def test_network_adjacency_matrix():
+    #test matrix taken from eqn 1 in the assignment pdf
+    input_adjacency_matrix = [[0, 1, 0, 3],
+                             [1, 0, 2, 1],
+                             [0, 2, 0, 0],
+                             [3, 1, 0, 0 ]]
+    test_network = Network(input_adjacency_matrix)
+
+    output_adjacency_matrix = test_network.adjacency_matrix
+    assert input_adjacency_matrix == output_adjacency_matrix
 
 
 # negative tests ensuring that the validity of inputs/raised errors work as expected:
