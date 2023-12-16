8375da7a2528598f402bb01971ce4a1c38b4285f
Edward Alexandroff <alexandroffe@gmail.com>
2023-12-16 16:21:39 +0000

    Test ensuring exceptions thrown when queries fail

--- test_webservice.py
+++ test_webservice.py
@@ -2,6 +2,8 @@ import pytest
 from unittest.mock import patch
 from journey_planner import main
 #from ..londontube.journey_planner import main #import from the correct place in directory
+from web_query import query_line_connectivity, station_information, update_matrix_disruption
+#from ..londontube.web_query import journey_planner
 
 #negative tests of the CLI:
 
@@ -18,3 +20,26 @@ def test_wrong_date_formats():
 
 #negative tests of the API:
 
+#testing that functions in journey_planner raise an appropriate exception when query unsuccessful (status_code!=200)
+def test_journey_planner_query_failure():
+    #testing the query_line_connectivity function
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.status_code = 500
+        with pytest.raises(Exception, match="Failed to get line connectivity information"):
+            query_line_connectivity(0)
+
+    #testing the station_information function
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.status_code = 500
+        with pytest.raises(Exception, match="Failed to get station information"):
+            station_information(0)
+
+    #testing the update_matrix_disruption function
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.status_code = 500
+        with pytest.raises(Exception, match="Failed to get disruption information"):
+            update_matrix_disruption([[0, 1], [1, 0]], date="01-01-2000")
+
+#testing that appropriate error thrown when information requested about station/list that doesn't exist
+
+#testing that appropriate error raised if user tries to fetch disruptions on a day that is too far in the future or past
