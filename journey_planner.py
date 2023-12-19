2e48cb94abb66d739e9287def5e2bd5796605a18
Hanze <hanzezhang0907@gmail.com>
2023-12-19 13:42:52 +0000

    add introduction and comment out test code

--- journey_planner.py
+++ journey_planner.py
@@ -7,13 +7,19 @@ from network import Network
 from web_query import station_information
 
 #Station index
-get_index = station_information('Vauxhall')[0][0]
+#get_index = station_information('Vauxhall')[0][0]
 
 #Station name
-get_name = station_information('Vauxhall')[0][1]
+#get_name = station_information('Vauxhall')[0][1]
 
 #Station latitude
-get_lat = station_information('Vauxhall')[0][2]
+#get_lat = station_information('Vauxhall')[0][2]
 
 #Station lontitude
-get_lon = station_information('Vauxhall')[0][3]
+#get_lon = station_information('Vauxhall')[0][3]
+
+"""
+This module provides functionality to plan journeys on the London Tube network.
+It includes functions to plot journey paths, clean data, and convert station indices to names.
+
+"""
