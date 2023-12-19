2fc900f40cd02dd2a9efb29727d9b78e2fefb840
Hanze <hanzezhang0907@gmail.com>
2023-12-19 13:18:26 +0000

    function to get new lines for disruption

--- web_query.py
+++ web_query.py
@@ -58,3 +58,42 @@ def station_information(line_id):
         stations_data.append([parts[0], station_name] + parts[2:])
         
     return stations_data
+
+def update_matrix_disruption(weight_matrix, date=None):
+
+    #Check if there is no date, return the original weight matrix
+    if date is None:
+        return weight_matrix
+    
+    #Feteching the distuption data from API
+    response = requests.get(f"https://rse-with-python.arc.ucl.ac.uk/londontube-service/disruptions/query?date={date}")
+    if response.status_code != 200:
+        raise Exception("Failed to get disruption information")
+    
+    disruptions = response.json()
+    for disruption in disruptions:
+        stations = disruption.get('stations')
+        
+        delay = disruption.get('delay', 1)  # Ensure delay is a valid number
+
+        if delay <= 0:  # Handle invalid delays, skip processing
+            continue
+
+        if len(stations) == 1: #Handle one station failure
+            station = stations[0]
+            weight_matrix[station, :] = np.inf  # Close all connections to this station
+            weight_matrix[:, station] = np.inf
+        
+        elif len(stations) == 2: #Handling delays between 2 stations
+            station1, station2 = stations # Ensuring station indices are within the bounds of the matrix
+            
+            if 0 <= station1 < len(weight_matrix) and 0 <= station2 < len(weight_matrix):
+                # Apply delay to the weights between the two stations
+                weight_matrix[station1][station2] *= delay
+                weight_matrix[station2][station1] *= delay
+                # If the resulting weights are zero or negative, set them to infinity (indicating no viable path)
+                if weight_matrix[station1][station2] <= 0 or weight_matrix[station2][station1] <= 0:  # Handle zero or negative weights
+                    weight_matrix[station1][station2] = np.inf
+                    weight_matrix[station2][station1] = np.inf
+
+    return weight_matrix
