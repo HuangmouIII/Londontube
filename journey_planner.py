dc64f2df38e08ee23eb639e88a89874067403d18
Hanze <hanzezhang0907@gmail.com>
2023-12-19 13:43:46 +0000

    function to get the name of station

--- journey_planner.py
+++ journey_planner.py
@@ -3,8 +3,8 @@ import argparse
 import matplotlib.pyplot as plt
 from londontube.web_query import journey_planner, station_information
 
-from network import Network
-from web_query import station_information
+#from network import Network
+#from web_query import station_information
 
 #Station index
 #get_index = station_information('Vauxhall')[0][0]
@@ -23,3 +23,15 @@ This module provides functionality to plan journeys on the London Tube network.
 It includes functions to plot journey paths, clean data, and convert station indices to names.
 
 """
+
+#Function to transfer the index to name
+def get_station_name(station_index):
+
+    stations_data = station_information('all')
+    station_names = [station[1] for station in stations_data] # Extract the names from station data
+
+
+    try:
+        return station_names[int(station_index)]
+    except (ValueError, IndexError): # If index is invalid or not found, return None
+        return None
